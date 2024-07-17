a59b03bfa7629d88e532107b1be29c1b1524b111
Arvid Reinert <arvid.reinert@gmail.com>
2024-07-17 14:37:30 +0200

    finished starting help

--- test_jam.py
+++ test_jam.py
@@ -2,8 +2,9 @@ from setup import *
 from rectangle import Rectangle
 button1 = Rectangle((width/5,height/15),(width/2,height/2),(0,0,0),"button.png")
 rect2 = Rectangle((50,50),(300,300),(255,255,255),False)
+running = True
 
-while True:
+while running:
     clock.tick(30)
     mous_pos = pygame.mouse.get_pos()
     rect2.rect_rect.center = mous_pos
@@ -22,6 +23,12 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if button1.get_point_colide(mous_pos):
                 print("click")
+                screen.fill((0,0,0))
+                rect3 = Rectangle((width/2,height/2),(width/2,height/2),(0,0,0),"test.webp")
+                rect3.update()
+                pygame.display.update()
+                running = False
+
     screen.fill((0,0,0))
     rect2.update()
     button1.update()
